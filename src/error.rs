671f4c4e09033c78285aaf23cb1c15e751ac1983
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-01-29 17:42:07 +0800

    Added starting implementation from `mizu`
    This is almost the same as the one in `mizu`, I'll modify it further to fix issues and add improvements into it.

---
+++ src/error.rs
@@ -0,0 +1,111 @@
+use std::{error::Error, fmt};
+
+use cpal::{
+    BackendSpecificError, DefaultStreamConfigError, PauseStreamError, PlayStreamError,
+    SupportedStreamConfigsError,
+};
+use rubato::ResamplerConstructionError;
+
+#[derive(Debug)]
+pub enum AudioPlayerError {
+    NoOutputDevice,
+    DualChannelNotSupported,
+    /// From cpal: The device associated with the stream is no longer available.
+    DeviceNotAvailable,
+    /// From cpal: See the [`BackendSpecificError`] docs for more information about this error variant.
+    DeviceBackendSpecificError(BackendSpecificError),
+    /// Forwarded from cpal
+    StreamTypeNotSupported,
+    /// Forwarded from cpal
+    StreamConfigInvalidArgument,
+    ResamplerConstructionError(ResamplerConstructionError),
+}
+
+impl Error for AudioPlayerError {}
+
+impl fmt::Display for AudioPlayerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::NoOutputDevice => write!(f, "No output device found"),
+            Self::DualChannelNotSupported => write!(f, "Dual channel not supported"),
+            Self::DeviceNotAvailable => write!(f, "Device not available"),
+            Self::DeviceBackendSpecificError(err) => {
+                write!(f, "Device backend specific error: {}", err)
+            }
+            Self::StreamTypeNotSupported => write!(f, "Stream type not supported"),
+            Self::StreamConfigInvalidArgument => write!(f, "Stream config invalid argument"),
+            Self::ResamplerConstructionError(err) => {
+                write!(f, "Resampler construction error: {}", err)
+            }
+        }
+    }
+}
+
+impl From<SupportedStreamConfigsError> for AudioPlayerError {
+    fn from(e: SupportedStreamConfigsError) -> Self {
+        match e {
+            SupportedStreamConfigsError::DeviceNotAvailable => Self::DeviceNotAvailable,
+            SupportedStreamConfigsError::InvalidArgument => Self::StreamConfigInvalidArgument,
+            SupportedStreamConfigsError::BackendSpecific { err } => {
+                Self::DeviceBackendSpecificError(err)
+            }
+        }
+    }
+}
+
+impl From<DefaultStreamConfigError> for AudioPlayerError {
+    fn from(e: DefaultStreamConfigError) -> Self {
+        match e {
+            DefaultStreamConfigError::DeviceNotAvailable => Self::DeviceNotAvailable,
+            DefaultStreamConfigError::StreamTypeNotSupported => Self::StreamTypeNotSupported,
+            DefaultStreamConfigError::BackendSpecific { err } => {
+                Self::DeviceBackendSpecificError(err)
+            }
+        }
+    }
+}
+
+impl From<ResamplerConstructionError> for AudioPlayerError {
+    fn from(e: ResamplerConstructionError) -> Self {
+        Self::ResamplerConstructionError(e)
+    }
+}
+
+#[derive(Debug)]
+pub enum PlayError {
+    /// From cpal: The device associated with the stream is no longer available.
+    DeviceNotAvailable,
+    /// From cpal: See the [`BackendSpecificError`] docs for more information about this error variant.
+    DeviceBackendSpecificError(BackendSpecificError),
+}
+
+impl Error for PlayError {}
+
+impl fmt::Display for PlayError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::DeviceNotAvailable => write!(f, "Device not available"),
+            Self::DeviceBackendSpecificError(err) => {
+                write!(f, "Device backend specific error: {}", err)
+            }
+        }
+    }
+}
+
+impl From<PlayStreamError> for PlayError {
+    fn from(e: PlayStreamError) -> Self {
+        match e {
+            PlayStreamError::DeviceNotAvailable => Self::DeviceNotAvailable,
+            PlayStreamError::BackendSpecific { err } => Self::DeviceBackendSpecificError(err),
+        }
+    }
+}
+
+impl From<PauseStreamError> for PlayError {
+    fn from(e: PauseStreamError) -> Self {
+        match e {
+            PauseStreamError::DeviceNotAvailable => Self::DeviceNotAvailable,
+            PauseStreamError::BackendSpecific { err } => Self::DeviceBackendSpecificError(err),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,200 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod error;
+
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use error::{AudioPlayerError, PlayError};
+use ringbuf::{HeapProducer, HeapRb};
+use rubato::{FftFixedInOut, Resampler};
+
+pub struct AudioPlayer {
+    buffer_producer: HeapProducer<f32>,
+    resampler: Option<FftFixedInOut<f32>>,
+    pre_resampled_buffer: Vec<f32>,
+    pre_resampled_split_buffers: [Vec<f32>; 2],
+    resample_process_buffers: [Vec<f32>; 2],
+    resampled_buffer: Vec<f32>,
+    output_stream: cpal::Stream,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl AudioPlayer {
+    pub fn new(sample_rate: u32) -> Result<Self, AudioPlayerError> {
+        let host = cpal::default_host();
+        let output_device = host
+            .default_output_device()
+            .ok_or(AudioPlayerError::NoOutputDevice)?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let sample_rate = cpal::SampleRate(sample_rate);
+
+        let conf = output_device
+            .supported_output_configs()?
+            .collect::<Vec<_>>();
+
+        let mut found_conf = false;
+
+        for c in &conf {
+            // must have 2 channels and f32 format
+            // (almost all? devices will have at least one configuration with these)
+            if c.channels() == 2
+                && c.sample_format() == cpal::SampleFormat::F32
+                && c.min_sample_rate() <= sample_rate
+                && c.max_sample_rate() >= sample_rate
+            {
+                found_conf = true;
+                break;
+            }
+        }
+
+        let (output_sample_rate, resampler) = if found_conf {
+            (sample_rate, None)
+        } else {
+            let def_conf = output_device.default_output_config()?;
+
+            if def_conf.channels() != 2 || def_conf.sample_format() != cpal::SampleFormat::F32 {
+                eprintln!("No supported configuration found for audio device, please open an issue in github `Amjad50/dynwave`\n\
+                      list of supported configurations: {:#?}", conf);
+                return Err(AudioPlayerError::DualChannelNotSupported);
+            }
+
+            (
+                def_conf.sample_rate(),
+                Some(FftFixedInOut::<f32>::new(
+                    sample_rate.0 as usize,
+                    def_conf.sample_rate().0 as usize,
+                    // the number of samples for one video frame in 60 FPS
+                    sample_rate.0 as usize / 60,
+                    2,
+                )?),
+            )
+        };
+
+        let config = cpal::StreamConfig {
+            channels: 2,
+            sample_rate: output_sample_rate,
+            buffer_size: cpal::BufferSize::Default,
+        };
+
+        // Limiting the number of samples in the buffer is better to minimize
+        // audio delay in emulation, this is because emulation speed
+        // does not 100% match audio playing speed (44100Hz).
+        // The buffer holds only audio for 1/4 second, which is good enough for delays,
+        // It can be reduced more, but it might cause noise(?) for slower machines
+        // or if any CPU intensive process started while the emulator is running
+        let buffer = HeapRb::new(output_sample_rate.0 as usize / 2);
+        let (buffer_producer, mut buffer_consumer) = buffer.split();
+
+        let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+            for sample in data {
+                *sample = buffer_consumer.pop().unwrap_or(0.);
+            }
+        };
+
+        let output_stream = output_device
+            .build_output_stream(&config, output_data_fn, Self::err_fn, None)
+            .expect("failed to build an output audio stream");
+
+        Ok(Self {
+            buffer_producer,
+            output_stream,
+            pre_resampled_buffer: Vec::new(),
+            pre_resampled_split_buffers: [Vec::new(), Vec::new()],
+            resample_process_buffers: [Vec::new(), Vec::new()],
+            resampled_buffer: Vec::new(),
+            resampler,
+        })
+    }
+
+    /// Start the player
+    pub fn play(&self) -> Result<(), PlayError> {
+        self.output_stream.play().map_err(|e| e.into())
+    }
+
+    /// Pause the player
+    pub fn pause(&self) -> Result<(), PlayError> {
+        self.output_stream.pause().map_err(|e| e.into())
+    }
+
+    pub fn queue(&mut self, data: &[f32]) {
+        let Some(resampler) = &mut self.resampler else {
+            // no resampling
+            self.buffer_producer.push_slice(data);
+            return;
+        };
+
+        // helper method to split channels into separate vectors
+        fn read_frames(inbuffer: &[f32], n_frames: usize, outputs: &mut [Vec<f32>]) {
+            for output in outputs.iter_mut() {
+                output.clear();
+                output.reserve(n_frames);
+            }
+            let mut value: f32;
+            let mut inbuffer_iter = inbuffer.iter();
+            for _ in 0..n_frames {
+                for output in outputs.iter_mut() {
+                    value = *inbuffer_iter.next().unwrap();
+                    output.push(value);
+                }
+            }
+        }
+
+        /// Helper to merge channels into a single vector
+        /// the number of channels is the size of `waves` slice
+        fn write_frames(waves: &[Vec<f32>], outbuffer: &mut Vec<f32>) {
+            let nbr = waves[0].len();
+            for frame in 0..nbr {
+                for wave in waves.iter() {
+                    outbuffer.push(wave[frame]);
+                }
+            }
+        }
+
+        self.pre_resampled_buffer.extend_from_slice(data);
+        // finish all the frames, as sometimes after appending many data
+        // we might get 2 loops worth of unprocessed audio
+        loop {
+            let frames = resampler.input_frames_next();
+
+            if self.pre_resampled_buffer.len() < frames * 2 {
+                return;
+            }
+
+            // only read the needed frames
+            read_frames(
+                &self.pre_resampled_buffer,
+                frames,
+                &mut self.pre_resampled_split_buffers,
+            );
+
+            self.resample_process_buffers[0].clear();
+            self.resample_process_buffers[0].clear();
+
+            let output_frames = resampler.output_frames_next();
+            self.resample_process_buffers[0].resize(output_frames, 0.);
+            self.resample_process_buffers[1].resize(output_frames, 0.);
+
+            resampler
+                .process_into_buffer(
+                    &self.pre_resampled_split_buffers,
+                    &mut self.resample_process_buffers,
+                    None,
+                )
+                .unwrap();
+
+            // resample
+            if self.resampled_buffer.len() < output_frames * 2 {
+                self.resampled_buffer
+                    .reserve(output_frames * 2 - self.resampled_buffer.len());
+            }
+            self.resampled_buffer.clear();
+            write_frames(&self.resample_process_buffers, &mut self.resampled_buffer);
+
+            self.buffer_producer.push_slice(&self.resampled_buffer);
+
+            self.pre_resampled_buffer = self.pre_resampled_buffer.split_off(frames * 2);
+        }
+    }
+}
+
+impl AudioPlayer {
+    fn err_fn(err: cpal::StreamError) {
+        eprintln!("an error occurred on audio stream: {}", err);
     }
 }
