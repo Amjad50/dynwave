a30a43623a5545c837bba5220d77745eac438240
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-01-29 18:07:28 +0800

    Reorganized resampler related code into `AudioResampler`
    This will make it together which is easier to manage.
    We also won't need to create `Vec` structs, even though it doesn't matter, as if we don't use them, they won't allocate.
    But its better this way

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,111 @@ use error::{AudioPlayerError, PlayError};
 use ringbuf::{HeapProducer, HeapRb};
 use rubato::{FftFixedInOut, Resampler, Sample};
 
-pub struct AudioPlayer<T: Sample> {
-    buffer_producer: HeapProducer<T>,
-    resampler: Option<FftFixedInOut<T>>,
+struct AudioResampler<T: Sample> {
+    resampler: FftFixedInOut<T>,
     pre_resampled_buffer: Vec<T>,
     pre_resampled_split_buffers: [Vec<T>; 2],
     resample_process_buffers: [Vec<T>; 2],
     resampled_buffer: Vec<T>,
+}
+
+impl<T: Sample + SizedSample> AudioResampler<T> {
+    fn new(input_rate: usize, output_rate: usize) -> Result<Self, AudioPlayerError> {
+        let resampler = FftFixedInOut::<T>::new(
+            input_rate,
+            output_rate,
+            // the number of samples for one video frame in 60 FPS
+            input_rate / 60,
+            2,
+        )?;
+
+        Ok(Self {
+            resampler,
+            pre_resampled_buffer: Vec::new(),
+            pre_resampled_split_buffers: [Vec::new(), Vec::new()],
+            resample_process_buffers: [Vec::new(), Vec::new()],
+            resampled_buffer: Vec::new(),
+        })
+    }
+
+    fn resample_into_producer(&mut self, data: &[T], producer: &mut HeapProducer<T>) {
+        // helper method to split channels into separate vectors
+        fn read_frames<T: Copy>(inbuffer: &[T], n_frames: usize, outputs: &mut [Vec<T>]) {
+            for output in outputs.iter_mut() {
+                output.clear();
+                output.reserve(n_frames);
+            }
+            let mut value: T;
+            let mut inbuffer_iter = inbuffer.iter();
+            for _ in 0..n_frames {
+                for output in outputs.iter_mut() {
+                    value = *inbuffer_iter.next().unwrap();
+                    output.push(value);
+                }
+            }
+        }
+
+        /// Helper to merge channels into a single vector
+        /// the number of channels is the size of `waves` slice
+        fn write_frames<T: Copy>(waves: &[Vec<T>], outbuffer: &mut Vec<T>) {
+            let nbr = waves[0].len();
+            for frame in 0..nbr {
+                for wave in waves.iter() {
+                    outbuffer.push(wave[frame]);
+                }
+            }
+        }
+
+        self.pre_resampled_buffer.extend_from_slice(data);
+        // finish all the frames, as sometimes after appending many data
+        // we might get 2 loops worth of unprocessed audio
+        loop {
+            let frames = self.resampler.input_frames_next();
+
+            if self.pre_resampled_buffer.len() < frames * 2 {
+                return;
+            }
+
+            // only read the needed frames
+            read_frames(
+                &self.pre_resampled_buffer,
+                frames,
+                &mut self.pre_resampled_split_buffers,
+            );
+
+            self.resample_process_buffers[0].clear();
+            self.resample_process_buffers[0].clear();
+
+            let output_frames = self.resampler.output_frames_next();
+            self.resample_process_buffers[0].resize(output_frames, T::EQUILIBRIUM);
+            self.resample_process_buffers[1].resize(output_frames, T::EQUILIBRIUM);
+
+            self.resampler
+                .process_into_buffer(
+                    &self.pre_resampled_split_buffers,
+                    &mut self.resample_process_buffers,
+                    None,
+                )
+                .unwrap();
+
+            // resample
+            if self.resampled_buffer.len() < output_frames * 2 {
+                self.resampled_buffer
+                    .reserve(output_frames * 2 - self.resampled_buffer.len());
+            }
+            self.resampled_buffer.clear();
+            write_frames(&self.resample_process_buffers, &mut self.resampled_buffer);
+
+            producer.push_slice(&self.resampled_buffer);
+
+            self.pre_resampled_buffer = self.pre_resampled_buffer.split_off(frames * 2);
+        }
+    }
+}
+
+pub struct AudioPlayer<T: Sample> {
+    buffer_producer: HeapProducer<T>,
+    resampler: Option<AudioResampler<T>>,
     output_stream: cpal::Stream,
 }
 
@@ -59,12 +157,9 @@ impl<T: Sample + SizedSample> AudioPlayer<T> {
 
             (
                 def_conf.sample_rate(),
-                Some(FftFixedInOut::<T>::new(
+                Some(AudioResampler::new(
                     sample_rate.0 as usize,
                     def_conf.sample_rate().0 as usize,
-                    // the number of samples for one video frame in 60 FPS
-                    sample_rate.0 as usize / 60,
-                    2,
                 )?),
             )
         };
@@ -97,10 +192,6 @@ impl<T: Sample + SizedSample> AudioPlayer<T> {
         Ok(Self {
             buffer_producer,
             output_stream,
-            pre_resampled_buffer: Vec::new(),
-            pre_resampled_split_buffers: [Vec::new(), Vec::new()],
-            resample_process_buffers: [Vec::new(), Vec::new()],
-            resampled_buffer: Vec::new(),
             resampler,
         })
     }
@@ -122,77 +213,7 @@ impl<T: Sample + SizedSample> AudioPlayer<T> {
             return;
         };
 
-        // helper method to split channels into separate vectors
-        fn read_frames<T: Copy>(inbuffer: &[T], n_frames: usize, outputs: &mut [Vec<T>]) {
-            for output in outputs.iter_mut() {
-                output.clear();
-                output.reserve(n_frames);
-            }
-            let mut value: T;
-            let mut inbuffer_iter = inbuffer.iter();
-            for _ in 0..n_frames {
-                for output in outputs.iter_mut() {
-                    value = *inbuffer_iter.next().unwrap();
-                    output.push(value);
-                }
-            }
-        }
-
-        /// Helper to merge channels into a single vector
-        /// the number of channels is the size of `waves` slice
-        fn write_frames<T: Copy>(waves: &[Vec<T>], outbuffer: &mut Vec<T>) {
-            let nbr = waves[0].len();
-            for frame in 0..nbr {
-                for wave in waves.iter() {
-                    outbuffer.push(wave[frame]);
-                }
-            }
-        }
-
-        self.pre_resampled_buffer.extend_from_slice(data);
-        // finish all the frames, as sometimes after appending many data
-        // we might get 2 loops worth of unprocessed audio
-        loop {
-            let frames = resampler.input_frames_next();
-
-            if self.pre_resampled_buffer.len() < frames * 2 {
-                return;
-            }
-
-            // only read the needed frames
-            read_frames(
-                &self.pre_resampled_buffer,
-                frames,
-                &mut self.pre_resampled_split_buffers,
-            );
-
-            self.resample_process_buffers[0].clear();
-            self.resample_process_buffers[0].clear();
-
-            let output_frames = resampler.output_frames_next();
-            self.resample_process_buffers[0].resize(output_frames, T::EQUILIBRIUM);
-            self.resample_process_buffers[1].resize(output_frames, T::EQUILIBRIUM);
-
-            resampler
-                .process_into_buffer(
-                    &self.pre_resampled_split_buffers,
-                    &mut self.resample_process_buffers,
-                    None,
-                )
-                .unwrap();
-
-            // resample
-            if self.resampled_buffer.len() < output_frames * 2 {
-                self.resampled_buffer
-                    .reserve(output_frames * 2 - self.resampled_buffer.len());
-            }
-            self.resampled_buffer.clear();
-            write_frames(&self.resample_process_buffers, &mut self.resampled_buffer);
-
-            self.buffer_producer.push_slice(&self.resampled_buffer);
-
-            self.pre_resampled_buffer = self.pre_resampled_buffer.split_off(frames * 2);
-        }
+        resampler.resample_into_producer(data, &mut self.buffer_producer);
     }
 
     fn err_fn(err: cpal::StreamError) {
